e5368a32259114a68d4033ed463daeddd2292bb2
KOVVURU DHANUSH <109410973+KovvuruDhanush@users.noreply.github.com>
2025-08-17 21:47:38 +0530

    Delete big-task directory

--- big-task/backend/college-admission-master/src/main/java/cfg/clg/exceptions/UnauthorizedException.java
+++
@@ -1,9 +0,0 @@
-package cfg.clg.exceptions;
-
-public class UnauthorizedException extends RuntimeException {
-
-
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
