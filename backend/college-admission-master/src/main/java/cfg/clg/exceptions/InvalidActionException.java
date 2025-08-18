6f7ecc35e9c4aa70f59dd4b72d48e2221b42553b
KOVVURU DHANUSH <109410973+KovvuruDhanush@users.noreply.github.com>
2025-08-18 08:01:08 +0530

    Delete backend/college-admission-master directory

--- backend/college-admission-master/src/main/java/cfg/clg/exceptions/InvalidActionException.java
+++
@@ -1,9 +0,0 @@
-package cfg.clg.exceptions;
-
-public class InvalidActionException extends Exception {
-
-	public InvalidActionException(String string) {
-		// TODO Auto-generated constructor stub
-	}
-
-}
